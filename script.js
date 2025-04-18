4eedc4b97c1282f8a86027638718b332240da8c1
stenowarriorstranscriptionsoftware <stenowarriortranscriptionsoftw@gmail.com>
2025-04-18 11:45:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 const firebaseConfig = {
   apiKey: "AIzaSyBjY-pE5jxQJgKqDZrcE7Im66_5r-X_mRA",
   authDomain: "setup-login-page.firebaseapp.com",
+  databaseURL: "https://setup-login-page-default-rtdb.firebaseio.com",
   projectId: "setup-login-page",
-  storageBucket: "setup-login-page.appspot.com",
+  storageBucket: "setup-login-page.firebasestorage.app",
   messagingSenderId: "341251531099",
   appId: "1:341251531099:web:f4263621455541ffdc3a7e",
   measurementId: "G-ZXFC7NR9HV"
 };
-
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const database = firebase.database();
